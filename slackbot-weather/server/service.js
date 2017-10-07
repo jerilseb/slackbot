de8616591db87876e5f0838702bb432e478c256c
Jeril Sebastian <seb988@gmail.com>
2017-10-07 13:44:04 +0530

    weather service added

---
+++ slackbot-weather/server/service.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const express = require('express');
+const service = express();
+const request = require('superagent');
+
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+
+service.get('/service/:location', (req, res, next) => {
+    
+    request.get(`http://api.openweathermap.org/data/2.5/weather?q=${req.params.location}&APPID=${WEATHER_API_KEY}&units=metric`, 
+    (err, response) => {
+
+        if (err) {
+            console.log(err);
+            return res.sendStatus(404);
+        }
+
+        res.json({result: `${response.body.weather[0].description} at ${response.body.main.temp} degrees`});
+
+    });
+});
+
+module.exports = service;
