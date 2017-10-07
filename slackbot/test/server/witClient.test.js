331c53fbad28f6ed6d9455766acf8cb3df8842d7
Jeril Sebastian <seb988@gmail.com>
2017-10-07 21:28:12 +0530

    wit client test cases

---
+++ slackbot/test/server/witClient.test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+require('should');
+const WitClient = require('../../src/server/witClient');
+
+describe('The WIT client', () => {
+    describe('ask', () => {
+        it('should return a valid wit response', (done) => {
+
+            const witToken = process.env.WIT_API_KEY;  
+            const witClient = new WitClient(witToken);
+
+            witClient.ask('What is the current time in Vienna?', (err, response) => {
+                if(err) return done(err);
+
+                response.intent[0].value.should.equal('time');
+                response.location[0].value.should.equal('Vienna');
+
+                return done();
+            });
+        }).timeout(3000);;
+    });
+});
